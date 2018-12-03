0dd2335c00345008f3059ba0ac173bba642611cb
Philip Japikse <skimedic@gmail.com>
2018-12-03 11:24:29 -0500

    Finished the demo for Brian

---
+++ XUnitTestProject1/ModelTests.cs
@@ -0,0 +1,42 @@
+using System;
+using Microsoft.AspNetCore.Identity.UI.Pages;
+using TestingDemo.Models;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class ModelTests : IDisposable
+    {
+        public ModelTests()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        [Fact]
+        public void ShouldShowRequestIdIfNotEmpty()
+        {
+            var sut = new ErrorViewModel {RequestId = "123"};
+            Assert.True(sut.ShowRequestId);
+        }
+        [Fact]
+        public void ShouldNotShowRequestIdIfEmpty()
+        {
+            var sut = new ErrorViewModel();
+            Assert.False(sut.ShowRequestId);
+        }
+
+        [Theory]
+        [InlineData("123", true)]
+        [InlineData("", false)]
+        public void ShouldShowRequestIdBasedOnValue(string requestId, bool expectedResult)
+        {
+            var sut = new ErrorViewModel { RequestId = requestId};
+            Assert.Equal(expectedResult,sut.ShowRequestId);
+        }
+    }
+}
---
+++ XUnitTestProject1/SimpleIntegrationTests.cs
@@ -0,0 +1,45 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using TestingDemo;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class SimpleIntegrationTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly HttpClient _client;
+
+        public SimpleIntegrationTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+            _factory.Dispose();
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Home/Index")]
+        [InlineData("/Home/About")]
+        [InlineData("/Home/Privacy")]
+        [InlineData("/Home/Contact")]
+        public async Task GetHomePage(string url)
+        {
+            // Act
+            var response = await _client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+    }
+}
